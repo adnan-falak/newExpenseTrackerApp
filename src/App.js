2b66ba116e57cd35d60fd7c57d0230cfca16845b
adnan-falak <adnanfalak2332@gmail.com>
2020-06-17 19:16:28 +0500

    Inatial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './components/Header';
+import {Balance} from './components/Balance';
+import {TransactionList} from './components/TransactionList';
+import {IncomeExpense} from './components/IncomeExpense';
+import {AddTransaction} from './components/AddTransaction';
+import {GlobalPorvider} from './context/GlobalState';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <GlobalPorvider>
+      <Header/>
+    <div className="container">
+      <Balance/>
+      <IncomeExpense/>
+      <TransactionList/>
+      <AddTransaction/>
+      
     </div>
+    </GlobalPorvider>
   );
 }
 
